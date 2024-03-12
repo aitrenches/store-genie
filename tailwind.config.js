612a9c5fd9ed1c8c327d8c841527ac95b4b9bebd
dharold1 <olamidedavid10@gmail.com>
2024-03-12 19:30:21 +0100

    hidden route

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   content: [
-    "./pages/*.js",
-    "./pages/**/*.js",
-    "./components/*.js",
-    "./components/**/*.js",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     boxShadow: {
