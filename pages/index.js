01f1b8bb6629afd3be87041c7cf7ce80552bb1b0
Anthony Oliko <anthonyoliko@gmail.com>
2024-06-13 14:15:32 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import SeoHead from "../components/SeoHead";
 export default function Home() {
   return (
     <>
-      <SeoHead title="Showluv Markets" />
+      <SeoHead title="Showluv" />
       <Layout>
         <Hero />
         <Feature />
